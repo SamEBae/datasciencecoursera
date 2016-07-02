d33db24bc1cfadfcc99b3b294e4457fa191d8b78
SamEBae <baesammie@gmail.com>
2016-07-02 10:56:23 -0700

    function for saving N gram

---
+++ Data Science Capstone/saveNGram.r
@@ -0,0 +1,78 @@
+library(tm)
+library(RWeka)
+library(SnowballC)
+
+set.seed(7)
+
+textDataBlogs   <- readLines("dataset/en_US/en_US.blogs.txt", encoding = "UTF-8", skipNul=TRUE)
+textDataTwitter <- readLines("dataset/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul=TRUE)
+textDataNews    <- readLines("dataset/en_US/en_US.news.txt", encoding = "UTF-8", skipNul=TRUE)
+
+sampleSize <- min(c(length(textDataTwitter)*0.25,length(textDataNews)*0.25,length(textDataBlogs)*0.25))
+
+sampleTwitter <- textDataTwitter[sample(1:length(textDataTwitter),sampleSize)]
+sampleNews <- textDataNews[sample(1:length(textDataNews),sampleSize)]
+sampleBlogs <- textDataBlogs[sample(1:length(textDataBlogs),sampleSize)]
+sampleTextVector <- c(sampleTwitter,sampleNews,sampleBlogs)
+
+writeLines(sampleTextVector, "sample/sample.txt")
+
+
+cname <- file.path(".", "sample")
+docs <- Corpus(DirSource(cname))
+
+# convert to lowercase
+docs <- tm_map(docs, content_transformer(tolower))
+
+# remove more transforms
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/|@|\\|")
+# remove punctuation
+docs <- tm_map(docs, removePunctuation)
+# remove numbers
+docs <- tm_map(docs, removeNumbers)
+# strip whitespace
+docs <- tm_map(docs, stripWhitespace)
+
+docs <- tm_map(docs, removeWords, stopwords("english"))
+
+docs <- tm_map(docs, stemDocument)
+
+
+# 1 word frequencies
+onewordTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
+onewordtm <- DocumentTermMatrix(docs, control = list(tokenize = onewordTokenizer))
+
+# 2 words frequencies
+twowordTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+twowordtm <- DocumentTermMatrix(docs, control = list(tokenize = twowordTokenizer))
+
+# 3 words frequencies
+threewordTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+threewordtm <- DocumentTermMatrix(docs, control = list(tokenize = threewordTokenizer))
+
+onewordtmfreq <- sort(colSums(as.matrix(onewordtm)), decreasing=TRUE)
+onewordfreq <- data.frame(word=names(onewordtmfreq), freq=onewordtmfreq)
+
+twowordtmfreq <- sort(colSums(as.matrix(twowordtm)), decreasing=TRUE)
+twowordfreq <- data.frame(word=names(twowordtmfreq), freq=twowordtmfreq)
+
+threewordtmfreq <- sort(colSums(as.matrix(threewordtm)), decreasing=TRUE)
+threewordfreq <- data.frame(word=names(threewordtmfreq), freq=threewordtmfreq)
+
+saveRDS(onewordtm, "onewordtm.rds")
+saveRDS(twowordtm, "twowordtm.rds")
+saveRDS(threewordtm, "threewordtm.rds")
+
+saveRDS(onewordfreq, "onewordfreq.rds")
+saveRDS(twowordfreq, "twowordfreq.rds")
+saveRDS(threewordfreq, "threewordfreq.rds")
+
+
+# cleaner n gram
+head(twowordfreq$word,4)
+twowordsample <- as.character(head(twowordfreq$word,4))
+twowords <- unlist(strsplit(twowordsample, " "))
+twowords
+
+
